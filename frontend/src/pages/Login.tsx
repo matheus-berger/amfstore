0df581f0b2e3ae08985e655281d0307ae8c35a61
Matheus Berger <contato.matheusberger@outlook.com>
2025-06-30 18:38:32 -0300

    feat(frontend): implenta login e menu de hamburguer

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,3 +1,59 @@
+import { useState, type FormEvent } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
 export function LoginPage() {
-  return <h1 className="text-2xl">Página de Login</h1>;
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const { signIn } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    await signIn({email, senha});
+    navigate('/');
+  }
+
+  return  (
+    <div className="max-w-md mx-auto mt-10">
+      <h1 className="text-3xl font-bold text-center mb-6">Login</h1>
+      <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-md">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Senha
+          </label>
+          <input
+            id="password"
+            type="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+          >
+            Entrar
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+
 }
